02bfe71d3aa952a12a9ea99d777c2860099f7c39
a1rzayev <asifrzayev2006@gmail.com>
2026-06-13 12:03:24 +0400

    Урок8(Логические, арифметические и условные операции)

---
+++ Python Junior/Урок8(Логические, арифметические и условные операции)/driver_licence_checker.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age: "))
+
+if(age >= 16 and age < 18):
+    print("you can drive motos")
+elif(age >= 18):
+    print("you can drive every vehichle")
+else:
+    print("you can drive only bicycle")
